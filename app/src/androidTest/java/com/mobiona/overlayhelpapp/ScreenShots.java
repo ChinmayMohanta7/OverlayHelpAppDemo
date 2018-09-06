2ecdb80cc1db22655f00ed3197c08cd708a90062
Chinmay Mohanta <chinu.moh@gmail.com>
2018-09-06 09:18:53 +0530

    Initial commit

---
+++ app/src/androidTest/java/com/mobiona/overlayhelpapp/ScreenShots.java
@@ -0,0 +1,35 @@
+package com.mobiona.overlayhelpapp;
+
+import android.support.test.espresso.Espresso;
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import tools.fastlane.screengrab.Screengrab;
+import tools.fastlane.screengrab.locale.LocaleTestRule;
+
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(JUnit4.class)
+public class ScreenShots {
+    @ClassRule
+    public static final LocaleTestRule localeTestRule = new LocaleTestRule();
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void testTakeScreenshot() {
+        Screengrab.screenshot("before_button_click");
+
+        // Your custom onView...
+        Espresso.onView(withId(R.id.textview)).perform(click());
+
+        Screengrab.screenshot("after_button_click");
+    }
+}
---
+++ app/src/main/java/com/mobiona/overlayhelpapp/MainActivity.java
@@ -0,0 +1,58 @@
+package com.mobiona.overlayhelpapp;
+
+import android.app.Dialog;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.TranslateAnimation;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //showOverlayHelp();
+        /*ImageView imageView=findViewById(R.id.arrow);
+        //Animation animation=AnimationUtils.loadAnimation(this,R.anim.arrow_animation);
+        Animation mAnimation = new TranslateAnimation(0, 100, 0, 0);
+        mAnimation.setDuration(700);
+        mAnimation.setFillAfter(true);
+        mAnimation.setRepeatCount(-1);
+        mAnimation.setRepeatMode(Animation.REVERSE);
+        imageView.startAnimation(mAnimation);
+*/
+        final TextView textView=findViewById(R.id.textview);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText("Jai Hind");
+            }
+        });
+    }
+
+    public void showOverlayHelp(){
+
+        final Dialog dialog = new Dialog(this,android.R.style.Theme_Black_NoTitleBar_Fullscreen);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+        dialog.setContentView(R.layout.overlay_dialog);
+        dialog.setCanceledOnTouchOutside(false);
+        Button cancelBtn = dialog.findViewById(R.id.cancel_btn);
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+    }
+}
